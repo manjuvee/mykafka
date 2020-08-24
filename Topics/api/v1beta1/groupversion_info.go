b97cb8339b5c58dc6a570dfe00e4350a6601d332
CharanKumar <charankumar.palla@gmail.com>
2020-08-24 23:03:35 +0530

    Correct domain name

--- Topics/api/v1beta1/groupversion_info.go
+++ Topics/api/v1beta1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1beta1 contains API Schema definitions for the kafka.strimzi.io v1beta1 API group
 // +kubebuilder:object:generate=true
-// +groupName=kafka.strimzi.io.my.domain
+// +groupName=kafka.strimzi.io
 package v1beta1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "kafka.strimzi.io.my.domain", Version: "v1beta1"}
+	GroupVersion = schema.GroupVersion{Group: "kafka.strimzi.io", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
